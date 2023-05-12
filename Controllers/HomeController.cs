ce4746a6f35b227f0552778ab12fdbb769ad6a20
Ryan Bharath <dbossman60@gmail.com>
2023-05-12 13:41:04 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokedexApp.Models;
+using Newtonsoft.Json;
+
+namespace PokedexApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly HttpClient _httpClient;
+
+    public HomeController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync("https://pokeapi.co/api/v2/pokemon/1");
+
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonContent = await response.Content.ReadAsStringAsync();
+            Pokemon? pokemon = JsonConvert.DeserializeObject<Pokemon>(jsonContent);
+            return View(pokemon);
+        }
+        else
+        {
+            return View("Error");
+        }
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,12 @@
+namespace PokedexApp.Models;
+
+public class Pokemon
+{
+    public int id {get; set;}
+
+    public string? name {get; set;}
+
+    public int weight {get; set;}
+
+    public int height {get; set;}
+}
